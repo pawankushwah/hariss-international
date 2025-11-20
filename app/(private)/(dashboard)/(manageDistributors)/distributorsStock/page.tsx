acbeae12f60762366593466ad43a7273502d52ae
Ankush Saxena <ankushsss99@gmail.com>
2025-11-20 18:48:47 +0530

    final changes

--- app/(private)/(dashboard)/(manageDistributors)/distributorsStock/page.tsx
+++ app/(private)/(dashboard)/(manageDistributors)/distributorsStock/page.tsx
@@ -224,56 +224,7 @@ const OverallPerformance: React.FC = () => {
                     <p className="text-xs text-gray-600 font-medium">{card.title}</p>
 
                     {/* Arrow + Percentage */}
-                    <div
-                      className={`flex items-center gap-1 ${card.isUp ? "text-green-500" : "text-red-500"
-                        }`}
-                    >
-
-
-
-                    </div>
-                    <div
-                      className={`flex items-center gap-1 ${card.isUp ? "text-green-500" : "text-red-500"
-                        }`}
-                    >
-                      {card.isUp ? (
-                        // Up Arrow
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={4}
-                          stroke="currentColor"
-                          className="w-3 h-3"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M4.5 15.75l7.5-7.5 7.5 7.5"
-                          />
-                        </svg>
-                      ) : (
-                        // Down Arrow
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={4}
-                          stroke="currentColor"
-                          className="w-3 h-3"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                          />
-                        </svg>
-                      )}
-
-                      <p className="text-xs font-medium">
-                        {Math.abs(card?.percentage)}%
-                      </p>
-                    </div>
+                   
 
                   </div>
 
